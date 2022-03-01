acd2d368b70c153e883949cf1f3491991eed6f32
azhim <ariefazhim@gmail.com>
2022-03-01 16:05:23 +0800

    qr scanner inserted

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  Container,
+  Card,
+  CardContent,
+  makeStyles,
+  Grid,
+} from "@material-ui/core"; // to import material UI
+import { QrReader } from "react-qr-reader";
 
 function App() {
+  const [data, setData] = useState("");
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className={classes.container}>
+      <Card>
+        <h2 className={classes.title}>Scan Qr Code with React</h2>
+        <CardContent>
+          <Grid container spacing={2}>
+            <br></br>
+            <br></br>
+            <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
+              <QrReader
+                delay={300}
+                style={{ width: "100%" }}
+                onResult={(result, error) => {
+                  if (!!result) {
+                    setData(result?.text);
+                  }
+
+                  if (!!error) {
+                    console.info(error);
+                  }
+                }}
+              />
+              <h3>Scanned Code:</h3>
+              <a>{data}</a>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Container>
   );
 }
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    marginTop: 10,
+  },
+  title: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    background: "#3f51b5",
+    color: "#fff",
+    padding: 20,
+  },
+  btn: {
+    marginTop: 10,
+    marginBottom: 20,
+  },
+}));
 export default App;
